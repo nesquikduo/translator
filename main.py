591c62839a8c0c551d866bacd808275412e6daf8
nesquikduo <nastya.margorina28@mail.ru>
2025-04-12 18:24:33 +0300

    добавила фреймы для текста и выборы языка программирования

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import tkinter as tk
+from ui_elements import create_text_area, create_combo
+
+languages_list = ["C++", "C#", "Java", "Ruby",
+                  "Pascal", "Java Script", "Swift",
+                  "1C", "Rust", "PHP", "Scala", "CSS",
+                  "Python", "HTML", "Не распознано"]
+
 
 root = tk.Tk()
 root.title("Языки программирования")
@@ -6,6 +13,11 @@ root.minsize(400, 400)
 left_frame = tk.Frame(root)
 right_frame = tk.Frame(root)
 button_frame = tk.Frame(root)
+
+combo_box_1 = create_combo(left_frame, languages_list)
+text_1 = create_text_area(left_frame)
+combo_box_2 = create_combo(right_frame, languages_list)
+text_2 = create_text_area(right_frame)
 
 left_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 button_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
---
+++ ui_elements.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
+
+def create_text_area(frame):
+    scrollbar = tk.Scrollbar(frame, orient=tk.VERTICAL)
+    text_area = tk.Text(frame, wrap=tk.WORD, yscrollcommand=scrollbar.set)
+    scrollbar.config(command=text_area.yview)
+    scrollbar.grid(row=1, column=1, sticky="ns", pady=5)
+    text_area.grid(row=1, column=0, pady=5, sticky="nsew")
+    return text_area
+
+def create_combo(frame, values):
+    combo = ttk.Combobox(frame, values=values, state="readonly")
+    combo.grid(row=0, column=0, pady=5, sticky="ew")
+    return combo
