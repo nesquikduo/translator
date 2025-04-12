80ad294e4e46a5560ef711c5fbc0f7364498429f
nesquikduo <nastya.margorina28@mail.ru>
2025-04-12 17:53:53 +0300

    добавила левый и правый фрейм и кнопку

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Языки программирования")
 root.minsize(400, 400)
+left_frame = tk.Frame(root)
+right_frame = tk.Frame(root)
+button_frame = tk.Frame(root)
+
+left_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+button_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
+right_frame.grid(row=0, column=2, padx=10, pady=10, sticky="nsew")
 root.mainloop()
