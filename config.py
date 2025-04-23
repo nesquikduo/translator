ea231899cd56147b50b1bdc7e92eae3b7261f588
nesquikduo <nastya.margorina28@mail.ru>
2025-04-23 18:35:24 +0300

    переделала правила перевода на Python

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ languages_colors = {
 }
 
 language_keywords = {
-    'C++': ['#include', 'std', '::', 'cout', 'cin', '->', '::', 'void'],
+    'C++': ['#include', 'std', '::', 'cout', 'cin', '->', 'void', 'int', 'return', 'if', 'else'],
     'C#': ['using', 'namespace', 'class', 'void', 'public', 'static'],
     'Java': ['public', 'class', 'void', 'import', 'System.out'],
     'Ruby': ['def', 'end', 'puts', 'class', 'module', 'begin'],
@@ -26,66 +26,120 @@ language_keywords = {
     'Rust': ['fn', 'let', 'mut', 'println!', 'impl', 'use'],
     'PHP': ['<?php', 'echo', '$', 'function', 'namespace', 'use'],
     'Scala': ['object', 'def', 'val', 'var', 'println', 'class'],
-    'Python': ['def', 'print', 'import', 'class', 'self', 'lambda']
+
+    'Python': ['def', 'print', 'input', 'import', 'class', 'self',
+                'lambda', 'if', 'else', 'elif', 'for', 'while',
+                'return', 'try', 'except', 'with', 'as', 'from', '#'
+]
 }
 
 translation_rules = {
+
     ('Python', 'C++'): {
         'def': 'void',
         'print': 'std::cout <<',
         'input': 'std::cin >>',
         'self': 'this',
-        ':': '{',
         'lambda': '[]()',
-        '#': '//'},
+        '#': '//',
+        ':': '{',
+        'return': 'return',
+        'True': 'true',
+        'False': 'false'},
+
     ('Python', 'Java'): {
         'def': 'public void',
         'print': 'System.out.println',
+        'input': 'Scanner.nextLine()',
         'self': 'this',
         'lambda': '->',
-        '#': '//'},
-    ('Python', 'Ruby'): {
-        'def': 'def',
-        'print': 'puts',
-        'self': 'self',
-        ':': '',
-        '#': '#'},
-    ('Python', 'PHP'): {
-        'def': 'function',
-        'print': 'echo',
-        'self': '$this',
-        '#': '//'},
+        '#': '//',
+        'return': 'return',
+        'True': 'true',
+        'False': 'false'},
+
     ('Python', 'Java Script'): {
         'def': 'function',
         'print': 'console.log',
+        'input': 'prompt',
         'self': 'this',
-        '#': '//'},
+        'lambda': '()=>',
+        '#': '//',
+        'return': 'return'},
+
+    ('Python', 'C#'): {
+        'def': 'void',
+        'print': 'Console.WriteLine',
+        'input': 'Console.ReadLine()',
+        'self': 'this',
+        'lambda': '=>',
+        '#': '//',
+        'return': 'return'},
+
+    ('Python', 'Ruby'): {
+        'def': 'def',
+        'print': 'puts',
+        'input': 'gets',
+        'self': 'self',
+        'lambda': '->',
+        '#': '#',
+        ':': '',
+        'return': 'return'},
+
     ('Python', 'Swift'): {
         'def': 'func',
         'print': 'print',
+        'input': 'readLine()',
         'self': 'self',
-        '#': '//'},
+        'lambda': 'in',
+        '#': '//',
+        'return': 'return'},
+
     ('Python', 'Rust'): {
         'def': 'fn',
         'print': 'println!',
+        'input': 'stdin',
         'self': 'self',
-        '#': '//'},
+        'lambda': '|x|',
+        '#': '//',
+        'return': 'return'},
+
     ('Python', 'Scala'): {
         'def': 'def',
         'print': 'println',
+        'input': 'scala.io.StdIn.readLine()',
         'self': 'this',
-        '#': '//'},
+        'lambda': '=>',
+        '#': '//',
+        'return': 'return'},
+
     ('Python', 'Pascal'): {
         'def': 'procedure',
         'print': 'writeln',
+        'input': 'readln',
         'self': '',
-        '#': '//'},
+        '#': '//',
+        'return': 'exit'},
+
     ('Python', '1C'): {
         'def': 'Процедура',
         'print': 'Сообщить',
+        'input': '',
         'self': 'ЭтотОбъект',
-        '#': '//'},
+        '#': '//',
+        'return': ''},
 
+    ('Python', 'PHP'): {
+        'def': 'function',
+        'print': 'echo',
+        'input': 'readline',
+        'self': '$this',
+        'lambda': 'function',
+        '#': '//',
+        'True': 'true',
+        'False': 'false',
+        'None': 'null',
+        'return': 'return'},
 
     # C++
     ('C++', 'Python'): {
